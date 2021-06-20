9c5151fe7408a263776e0f22d075b3448a4d6d9d
abhinav-TB <abhinavtb@gmail.com>
2021-06-20 20:15:05 +0530

    updated model

--- model.py
+++ model.py
@@ -4,24 +4,13 @@ import torch.nn as nn
 class VGG(nn.Module):
     def __init__(self):
         super(VGG, self).__init__()
-        # The first number x in convx_y gets added by 1 after it has gone
-        # through a maxpool, and the second y if we have several conv layers
-        # in between a max pool. These strings (0, 5, 10, ..) then correspond
-        # to conv1_1, conv2_1, conv3_1, conv4_1, conv5_1 mentioned in NST paper
         self.chosen_features = ["0", "5", "10", "19", "28"]
 
-        # We don't need to run anything further than conv5_1 (the 28th module in vgg)
-        # Since remember, we dont actually care about the output of VGG: the only thing
-        # that is modified is the generated image (i.e, the input).
         self.model = models.vgg19(pretrained=True).features[:29]
 
     def forward(self, x):
-        # Store relevant features
         features = []
 
-        # Go through each layer in model, if the layer is in the chosen_features,
-        # store it in features. At the end we'll just return all the activations
-        # for the specific layers we have in chosen_features
         for layer_num, layer in enumerate(self.model):
             x = layer(x)
 
